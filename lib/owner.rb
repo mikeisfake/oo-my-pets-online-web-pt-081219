0dd8e91177a1a1b1f2941f2e9c952cd646197459
Mike Cooper <mikescooper@icloud.com>
2019-09-16 06:19:19 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,44 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_reader :name , :species
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self 
+  end 
+  
+  def say_species
+    "I am a #{species}."
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def self.count
+    self.all.length 
+  end
+  
+  def self.reset_all 
+    self.all.clear
+  end 
+  
+  def cats 
+    Cat.all.select { |cat| cat.owner == self }
+  end 
+  
+  def dogs 
+    Dog.all.select { |dog| dog.owner == self }
+  end 
+  
+  def buy_cat(name)
+    new_cat = Cat.new(name, self.name)
+    Cat.all << new_cat
+    binding.pry 
+  end
+  
 end
